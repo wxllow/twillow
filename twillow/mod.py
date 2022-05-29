289326f9e5553b008c543b7d8dbfe5ea53290e50
wxllow <willow@wxllow.dev>
2022-05-29 12:09:26 -0400

    Voice handlers

--- twillow/mod.py
+++ twillow/mod.py
@@ -45,3 +45,45 @@ def load_module(loc, type=None):
         name = module.name or os.path.split(loc)[-1].split('.')[0]
 
         return name, module.new()
+
+
+def load_voice_handler(loc, type=None):
+    """Load voice handler, returns voice handler class"""
+    if not type:
+        if loc.endswith('.py'):
+            type = 'py'
+        else:
+            type = 'lua'
+
+    if type == 'py':
+        name = os.path.split(loc)[-1].split('.')[0]
+
+        # Load module
+        spec = importlib.util.spec_from_file_location(
+            f"_voicehandler.{name}", loc)
+
+        py_module = importlib.util.module_from_spec(spec)
+
+        spec.loader.exec_module(py_module)
+
+        module = py_module.voice_handler()
+
+        # Return module
+        if 'name' in dir(module):
+            name = str(module.name)
+
+        return name, module()
+    else:
+        # Execute lau module from file
+        lua = LuaRuntime(unpack_returned_tuples=True)
+
+        with open(loc, 'r') as f:
+            lua.execute(f.read())
+
+        g = lua.globals()
+
+        # Return module
+        module = g.voice_handler()
+        name = module.name or os.path.split(loc)[-1].split('.')[0]
+
+        return name, module.new()
